52a7a5c4656e2944a1e7da40ac455014fb97f6b8
plamspetkov <plams.petkov@gmail.com>
2021-11-21 21:53:51 +0100

    Figuring out the JS

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
-'use strict';
+// let li = document.querySelectorAll('.faq_text li');
+// for (var i = 0; i < li.length; i++) {
+//   li[i].addEventListener('click', (e) => {
+//     let clickedLi;
+//     if (e.target.classList.contains('question')) {
+//       clickedLi = e.target.parentElement;
+//     } else {
+//       clickedLi = e.target.parentElement.parentElement;
+//     }
+//     clickedLi.classList.toggle('showAnswer');
+//   });
+// }
 
-const question = document.querySelectorAll('.question');
-const questionTitle = document.querySelectorAll('.question_title');
-const arrow = document.querySelectorAll('.arrow');
-const text = document.querySelectorAll('.text');
+// const btn = document.querySelectorAll('.question_btn');
 
-for (let i = 0; i < question.length; i++) {
-  question[i].addEventListener('click', function () {
-    console.log(question);
-    for (let q = 0; q < text.length; q++) {
-      if (i === q) {
-        text[q].classList.add('.show');
-        arrow[q].classList.add('.rotate');
-        questionTitle[q].classList.toggle('.active');
-      } else {
-        text[q].classList.remove('.show');
-        arrow[q].classList.remove('.rotate');
-        questionTitle[q].classList.remove('.active');
-      }
-    }
-  });
+// btn.forEach((element) => {
+// 	element.addEventListener('click', (e) => {
+// 		let clickLi;
+// 		if (e.target.classList.contains('answer')) {
+// 			clickLi = e.target.parentElement;
+// 		} else {
+// 			clickLi = e.target.parentElement.parentElement;
+// 		}
+// 		clickLi.classList.toggle('answer_show');
+// 	});
+// });
+
+// let acc = document.getElementsByClassName('question_btn');
+// for (let i = 0; i < acc.length; i++) {
+// 	acc[i].addEventListener('click', function () {
+// 		this.classList.toggle('active');
+// 		let answer = this.nextElementSibling;
+// 		if (answer.style.display === 'block') {
+// 			answer.style.display = 'none';
+// 		} else {
+// 			answer.style.display = 'block';
+// 			acc.classList.add('.question_btn:active');
+// 		}
+// 	});
+// }
+
+let acc = document.getElementsByClassName('question_btn');
+for (let i = 0; i < acc.length; i++) {
+	acc[i].addEventListener('click', function () {
+		// this.classList.toggle('active');
+		let answer = this.nextElementSibling;
+		if (answer.classList.contains('answer')) {
+			answer.classList.remove('answer');
+			answer.classList.add('answer_show');
+		} else {
+			answer.classList.add('answer');
+		}
+	});
 }
