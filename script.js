fc76c883195813b6e33bb6e53425acc3df43daee
plamspetkov <plams.petkov@gmail.com>
2021-11-22 10:10:59 +0100

    Open accordion done

--- script.js
+++ script.js
@@ -10,45 +10,12 @@
 //     clickedLi.classList.toggle('showAnswer');
 //   });
 // }
-
-// const btn = document.querySelectorAll('.question_btn');
-
-// btn.forEach((element) => {
-// 	element.addEventListener('click', (e) => {
-// 		let clickLi;
-// 		if (e.target.classList.contains('answer')) {
-// 			clickLi = e.target.parentElement;
-// 		} else {
-// 			clickLi = e.target.parentElement.parentElement;
-// 		}
-// 		clickLi.classList.toggle('answer_show');
-// 	});
-// });
-
-// let acc = document.getElementsByClassName('question_btn');
-// for (let i = 0; i < acc.length; i++) {
-// 	acc[i].addEventListener('click', function () {
-// 		this.classList.toggle('active');
-// 		let answer = this.nextElementSibling;
-// 		if (answer.style.display === 'block') {
-// 			answer.style.display = 'none';
-// 		} else {
-// 			answer.style.display = 'block';
-// 			acc.classList.add('.question_btn:active');
-// 		}
-// 	});
-// }
-
-let acc = document.getElementsByClassName('question_btn');
-for (let i = 0; i < acc.length; i++) {
-	acc[i].addEventListener('click', function () {
-		// this.classList.toggle('active');
-		let answer = this.nextElementSibling;
-		if (answer.classList.contains('answer')) {
-			answer.classList.remove('answer');
-			answer.classList.add('answer_show');
-		} else {
-			answer.classList.add('answer');
-		}
+const button = document.querySelectorAll('.question_btn');
+const acc = document.querySelectorAll('.question');
+button.forEach((button) => {
+	button.addEventListener('click', function handleClick(e) {
+		const isOpen =
+			e.target.closest('.question').classList.toggle('is-open') &&
+			button.classList.toggle('is-open');
 	});
-}
+});
