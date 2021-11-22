db1ee54ba9841d61de1aa7e11ac9e8429c0024d7
plamspetkov <plams.petkov@gmail.com>
2021-11-22 19:44:27 +0100

    accordion

--- script.js
+++ script.js
@@ -3,8 +3,5 @@ const acc = document.querySelectorAll('.question');
 button.forEach((button) => {
 	button.addEventListener('click', function handleClick(e) {
 		const isOpen = e.target.closest('.question').classList.toggle('is-open');
-		acc.forEach((acc) => {
-			acc.classList.remove('is-open');
-		});
 	});
 });
